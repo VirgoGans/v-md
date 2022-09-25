28f5b73637a55ccb0725d392f8ace86448749ef2
VirgoGans <77391671+VirgoGans@users.noreply.github.com>
2022-09-25 19:39:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import makeWASocket, {useSingleFileAuthState} from '@adiwajshing/baileys'
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 process.on('uncaughtException', console.error);
 require('./config')
